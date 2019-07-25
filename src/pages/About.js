0275a753ddd82b8078177f59e853e54a8f9fdd0b
Joel Rocha <joelxr@gmail.com>
2019-07-25 02:15:55 -0300

    About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,8 +7,25 @@ class About extends React.Component {
       <Consumer>
         {() => (
           <div className="about">
-            <h1>Motivation</h1>
-            <p>This is YBMS (Your Beloved Movies and Series).</p>
+            <div className="aboutContent">
+              <h1>About the YBMS</h1>
+              <p>
+                This is YBMS (Your Beloved Movies and Series), here you can
+                easilly have a list of your top movies and TV Series and create
+                a list of favourites.
+              </p>
+              <p>
+                Besides that, you can see all important data about the movie and
+                TV serie you like.
+              </p>
+
+              <h1>About the author</h1>
+              <p>
+                I'm <u>Joel Rocha</u> and I like to create WEB sites like this.
+                I've been a developer for over ten years and I'm always looking
+                foward to work on challeging projects.
+              </p>
+            </div>
           </div>
         )}
       </Consumer>
